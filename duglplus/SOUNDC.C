cfaa00e42cc319944975c7ca3cb61d73850760a1
Fakhri Feki <fakhri_fki@hotmail.com>
2026-03-26 02:12:40 +0100

    SOUND: add function CreateDVoice, update sound driver struct.

--- duglplus/SOUNDC.C
+++ duglplus/SOUNDC.C
@@ -78,68 +78,137 @@ void DestroySoundDRV(SoundDRV *SndDrv)
 	memset(SndDrv,0,SndDrv->SizeDrv);
 }
 
-
-int  LoadWAV(DVoice *Vc,char *Fname)
-{	FILE *InWAV;
+int  LoadWAV(DVoice **Vc,char *Fname) {
+	FILE *InWAV;
 	HeadWAV hwav;
 	void *Buff;
-	memset(Vc, 0, sizeof(DVoice));
+	*Vc = (DVoice*) malloc(sizeof(DVoice));
+	if (*Vc == NULL)
+		return 0; // no mem
+
+	memset(*Vc, 0, sizeof(DVoice));
 	if ((InWAV=fopen(Fname,"rb"))==NULL) return 0;
 	fread(&hwav,sizeof(HeadWAV),1,InWAV);
 	if (hwav.Sign!='FFIR' || hwav.SignDATA!='atad' ||
-	    (hwav.BitEchant!=8 && hwav.BitEchant!=16) )
-	  { fclose(InWAV); return 0; }
+	    (hwav.BitEchant!=8 && hwav.BitEchant!=16) ) {
+		free(*Vc);
+		fclose(InWAV);
+		return 0;
+	}
 
-	if ((Buff=malloc(hwav.SizeDATA))==NULL)
-	  { fclose(InWAV); return 0; }
+	if ((Buff=malloc(hwav.SizeDATA))==NULL) {
+		free(*Vc);
+		fclose(InWAV);
+		return 0;
+	}
 	if (hwav.Type==1) {
-	  if (hwav.BitEchant==8) Vc->Type=0;
-	     else Vc->Type=2;
+	  if (hwav.BitEchant==8) (*Vc)->Type=0;
+	     else (*Vc)->Type=2;
 	} else if (hwav.Type==2) {
-	  if (hwav.BitEchant==8) Vc->Type=1;
-	     else Vc->Type=3;
+	  if (hwav.BitEchant==8) (*Vc)->Type=1;
+	     else (*Vc)->Type=3;
 	} else { free(Buff); fclose(InWAV); return 0; }
 
-	if (fread(Buff,hwav.SizeDATA,1,InWAV)<1)
-	  { free(Buff); fclose(InWAV); return 0; }
-	Vc->Ptr=Buff;
-	Vc->Size=hwav.SizeDATA;
-	Vc->Freq=hwav.SamplingSpeed;
-	Vc->SizeSecond=hwav.ByteOutSec;
+	if (fread(Buff,hwav.SizeDATA,1,InWAV)<1) {
+		free(*Vc);
+		free(Buff);
+		fclose(InWAV);
+		return 0;
+	}
+	(*Vc)->Ptr=Buff;
+	(*Vc)->Size=hwav.SizeDATA;
+	(*Vc)->Freq=hwav.SamplingSpeed;
+	(*Vc)->SizeSecond=hwav.ByteOutSec;
 
 	fclose(InWAV);
 	return 1;
 }
 
-int  LoadMemWAV(DVoice *Vc,void *In,int SizeIn)
+int  LoadMemWAV(DVoice **Vc,void *In,int SizeIn)
 {	HeadWAV hwav;
-	void *Buff;
-	Vc->Ptr=0;
+	void *Buff = NULL;
 	memcpy(&hwav,In,sizeof(HeadWAV));
 	if (hwav.Sign!='FFIR' || hwav.SignDATA!='atad' ||
-	    (hwav.BitEchant!=8 && hwav.BitEchant!=16) )
-	  return 0;
+	    (hwav.BitEchant!=8 && hwav.BitEchant!=16) ) {
+		return 0;
+	}
+	*Vc = (DVoice*) malloc(sizeof(DVoice));
+	if (*Vc == NULL)
+		return 0; // no mem
+	memset(*Vc, 0, sizeof(DVoice));
 
-	if ((Buff=malloc(hwav.SizeDATA))==NULL) return 0;
+	if ((Buff=malloc(hwav.SizeDATA))==NULL) {
+		free(*Vc);
+		*Vc = NULL;
+		return 0;
+	}
 	if (hwav.Type==1) {
-	  if (hwav.BitEchant==8) Vc->Type=0;
-	     else Vc->Type=2;
+	  if (hwav.BitEchant==8) (*Vc)->Type=0;
+	     else (*Vc)->Type=2;
 	} else if (hwav.Type==2) {
-	  if (hwav.BitEchant==8) Vc->Type=1;
-	     else Vc->Type=3;
-	} else { free(Buff); return 0; }
-	if ((sizeof(HeadWAV)+hwav.SizeDATA)<SizeIn)
-	  { free(Buff); return 0; }
+	  if (hwav.BitEchant==8) (*Vc)->Type=1;
+	     else (*Vc)->Type=3;
+	} else {
+		free(Buff);
+		return 0;
+	}
+	if ((sizeof(HeadWAV)+hwav.SizeDATA)<SizeIn) {
+		free(*Vc);
+		*Vc = NULL;
+		free(Buff);
+		return 0;
+	}
 	memcpy(Buff,In+sizeof(HeadWAV),hwav.SizeDATA);
-	Vc->Ptr=Buff;
-	Vc->Size=hwav.SizeDATA;
-	Vc->Freq=hwav.SamplingSpeed;
-	Vc->SizeSecond=hwav.ByteOutSec;
+	(*Vc)->Ptr=Buff;
+	(*Vc)->Size=hwav.SizeDATA;
+	(*Vc)->Freq=hwav.SamplingSpeed;
+	(*Vc)->SizeSecond=hwav.ByteOutSec;
 
 	return 1;
 }
 
-void DestroyVoice(DVoice *Vc) {
-	if (Vc->Ptr!=NULL) { free(Vc->Ptr); Vc->Ptr=NULL; }
+int  CreateDVoice(DVoice **Vc, bool bits16, bool stereo, int sampleSpeed, int countSamples) {
+	static int typeToSampleSize[] = { 1, 2, 2, 4 };
+	void *Buff = NULL;
+
+	if (sampleSpeed < 1 || countSamples < 1)
+		return 0;
+	*Vc = (DVoice*) malloc(sizeof(DVoice));
+	if (*Vc == NULL)
+		return 0; // no mem
+	memset(*Vc, 0, sizeof(DVoice));
+
+	if (stereo) { // stereo
+		if (!bits16)
+			(*Vc)->Type=1;
+	    else
+			(*Vc)->Type=3;
+	} else { // mono
+		if (!bits16)
+			(*Vc)->Type=0;
+	    else
+			(*Vc)->Type=2;
+	}
+	(*Vc)->Size = countSamples * typeToSampleSize[(*Vc)->Type];
+	if ((Buff=malloc((*Vc)->Size))==NULL) {
+		free(*Vc);
+		*Vc = NULL;
+		return 0;
+	}
+	memset(Buff, 0, (*Vc)->Size);
+	(*Vc)->Ptr=Buff;
+	(*Vc)->Freq = sampleSpeed;
+	(*Vc)->SizeSecond =  sampleSpeed * typeToSampleSize[(*Vc)->Type];
+	return 1;
+}
+
+void DestroyDVoice(DVoice *Vc) {
+	if (Vc != NULL) {
+		if (Vc->Ptr!=NULL) {
+			free(Vc->Ptr);
+			Vc->Ptr=NULL;
+		}
+		free(Vc);
+	}
 }
 
